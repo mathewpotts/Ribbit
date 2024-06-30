765a73036dcdcd80b26d070491923227581b2c24
Mathew Potts <potts.matt.93@gmail.com>
2024-06-30 16:03:02 -0600

    Added oauth token usage

--- preload.py
+++ preload.py
@@ -13,13 +13,13 @@ def preload_songs(youtube_url):
     queue = []
     try:
         if 'playlist' in youtube_url:
-            pl = Playlist(youtube_url)
+            pl = Playlist(youtube_url, use_oauth=True)
             titles = [video.title for video in pl.videos]
             video_urls = [video.streams.filter(progressive=True, file_extension='mp4').first().url for video in pl.videos]
             for i, video_url in enumerate(video_urls):
                 queue.append([titles[i], video_url])
         else:
-            yt = YouTube(youtube_url)
+            yt = YouTube(youtube_url, use_oauth=True)
             title = yt.title
             video_url = yt.streams.filter(progressive=True, file_extension='mp4').first().url
             queue.append([title, video_url])
